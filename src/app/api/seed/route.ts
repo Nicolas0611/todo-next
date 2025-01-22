074085c19425fd96d464988e0d795ea625cb5daf
Nicolás Sierra <nico.sierra13@gmail.com>
2025-01-21 22:11:24 -0500

    first commit

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const seedData = [
+    {
+      description: "Piedra del Alma",
+      complete: true,
+    },
+    {
+      description: "Piedra del Poder",
+    },
+    {
+      description: "Piedra del Tiempo",
+    },
+    {
+      description: "Piedra del Espacio",
+    },
+    {
+      description: "Piedra del Realidad",
+    },
+  ];
+
+  await prisma.todo.deleteMany();
+  const todo = await prisma.todo.createMany({
+    data: seedData,
+  });
+  console.log(todo);
+
+  return NextResponse.json({
+    message: "Seed executed",
+  });
+}
