03ec8e7438be3fe0d17a51144c487ff85153a762
Nicolás Sierra <nico.sierra13@gmail.com>
2025-01-27 22:49:50 -0500

    Add revalidate for cache

--- src/app/dashboard/server-todos/page.tsx
+++ src/app/dashboard/server-todos/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "auto";
+export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 import prisma from "@/lib/prisma";
@@ -9,10 +9,11 @@ export const metadata = {
   description: "SEO Title",
 };
 
-export default async function PageTodosPage() {
+export default async function ServerTodosPage() {
   const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
   return (
     <div>
+      <p>Server actions</p>
       <div className="w-full px-3 mx-5 mb-5">
         <NewTodo />
       </div>
