2b8861987e04cda09d6b86fcc409531a5f94a7a4
Nicolás Sierra <nico.sierra13@gmail.com>
2025-03-06 22:43:51 -0500

    Add cookies behaviour to set to default selected value

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,16 +1,21 @@
 import { TabBar } from "@/components";
+import { cookies } from "next/headers";
 
 export const metadata = {
   title: "Cookies Page",
   description: "SEO Title",
 };
 
-export default function CookiesPage() {
+export default async function CookiesPage() {
+  const cookieStore = await cookies();
+  const tab = Number(cookieStore.get("selectedTab")?.value ?? 1);
+
+  // const allCookies = cookieStore.getAll();
   return (
     <div className="grid grid-cols-1 sm:grid-col gap-3">
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={tab} />
       </div>
     </div>
   );
