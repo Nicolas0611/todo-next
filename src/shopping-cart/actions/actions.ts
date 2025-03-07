0a4a459c424b77cd772842fe76112fd380845b69
Nicolás Sierra <nico.sierra13@gmail.com>
2025-03-07 14:26:34 -0500

    Add cookieCard logic

---
+++ src/shopping-cart/actions/actions.ts
@@ -0,0 +1,20 @@
+import { getCookie, hasCookie, setCookie } from "cookies-next";
+
+export const getCookieCart = (): { [id: string]: number } => {
+  if (hasCookie("cart")) {
+    const cookieCart = JSON.parse((getCookie("cart") as string) ?? "{}");
+    return cookieCart;
+  }
+  return {};
+};
+
+export const addProductToCard = (id: string) => {
+  const cookieCart = getCookieCart();
+  if (cookieCart[id]) {
+    cookieCart[id] += 1;
+  } else {
+    cookieCart[id] = 1;
+  }
+  console.log({ cookieCart: cookieCart[id] });
+  setCookie("cart", JSON.stringify(cookieCart));
+};
